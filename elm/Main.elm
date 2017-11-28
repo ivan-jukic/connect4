35e470e88cf4179da26f1dad3fbdfbcfbd03c941
Ivan <ivan.jukic14@gmail.com>
2017-11-28 01:25:22 +0000

    cube example

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import AnimationFrame
+import Color exposing (Color)
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (width, height, style)
 import Math.Matrix4 as Mat4 exposing (Mat4)
@@ -118,19 +119,32 @@ view model =
             [ WebGL.entity
                 vertexShader
                 fragmentShader
-                mesh
-                { perspective = perspective (model.t / 1000) }
+                cubeMesh
+                (uniforms (model.t / 5000))
             ]
         ]
 
 
 {-|
 -}
-perspective : Float -> Mat4
-perspective t =
-    Mat4.mul
-        (Mat4.makePerspective 45 1 0.01 100)
-        (Mat4.makeLookAt (vec3 (4 * cos t) 0 (4 * sin t)) (vec3 0 0 0) (vec3 0 1 0))
+type alias Uniforms =
+    { rotation : Mat4
+    , perspective : Mat4
+    , camera : Mat4
+    , shade : Float
+    }
+
+
+uniforms : Float -> Uniforms
+uniforms theta =
+    { rotation =
+        Mat4.mul
+            (Mat4.makeRotate (3 * theta) (vec3 0 1 0))
+            (Mat4.makeRotate (2 * theta) (vec3 1 0 0))
+    , perspective = Mat4.makePerspective 45 1 0.01 100
+    , camera = Mat4.makeLookAt (vec3 0 0 5) (vec3 0 0 0) (vec3 0 1 0)
+    , shade = 0.8
+    }
 
 
 -- Mesh
@@ -139,30 +153,73 @@ perspective t =
 {-|
 -}
 type alias Vertex =
-    { position : Vec3
-    , color : Vec3
+    { color : Vec3
+    , position : Vec3
     }
 
 
 {-|
 -}
-mesh : Mesh Vertex
-mesh =
-    WebGL.triangles
-        [ ( Vertex (vec3 0 0 0) (vec3 1 0 0)
-          , Vertex (vec3 1 1 0) (vec3 0 1 0)
-          , Vertex (vec3 1 -1 0) (vec3 0 0 1)
-          )
-        ]
+cubeMesh : Mesh Vertex
+cubeMesh =
+    let
+        rft =
+            vec3 1 1 1
+
+        lft =
+            vec3 -1 1 1
+
+        lbt =
+            vec3 -1 -1 1
+
+        rbt =
+            vec3 1 -1 1
+
+        rbb =
+            vec3 1 -1 -1
+
+        rfb =
+            vec3 1 1 -1
+
+        lfb =
+            vec3 -1 1 -1
+
+        lbb =
+            vec3 -1 -1 -1
+    in
+    [ face Color.green rft rfb rbb rbt
+    , face Color.blue rft rfb lfb lft
+    , face Color.yellow rft lft lbt rbt
+    , face Color.red rfb lfb lbb rbb
+    , face Color.purple lft lfb lbb lbt
+    , face Color.orange rbt rbb lbb lbt
+    ]
+    |> List.concat
+    |> WebGL.triangles
+
+
+face : Color -> Vec3 -> Vec3 -> Vec3 -> Vec3 -> List ( Vertex, Vertex, Vertex )
+face rawColor a b c d =
+    let
+        color =
+            let
+                c =
+                    Color.toRgb rawColor
+            in
+                vec3
+                    (toFloat c.red / 255)
+                    (toFloat c.green / 255)
+                    (toFloat c.blue / 255)
+
+        vertex position =
+            Vertex color position
+    in
+    [ ( vertex a, vertex b, vertex c )
+    , ( vertex c, vertex d, vertex a )
+    ]
 
 
 -- Shaders
-
-
-{-|
--}
-type alias Uniforms =
-    { perspective : Mat4 }
 
 
 {-|
@@ -174,10 +231,12 @@ vertexShader =
         attribute vec3 position;
         attribute vec3 color;
         uniform mat4 perspective;
+        uniform mat4 camera;
+        uniform mat4 rotation;
         varying vec3 vcolor;
 
         void main () {
-            gl_Position = perspective * vec4(position, 1.0);
+            gl_Position = perspective * camera * rotation * vec4(position, 1.0);
             vcolor = color;
         }
 
@@ -191,10 +250,11 @@ fragmentShader =
     [glsl|
     
         precision mediump float;
+        uniform float shade;
         varying vec3 vcolor;
 
         void main () {
-            gl_FragColor = vec4(vcolor, 1.0);
+            gl_FragColor = shade * vec4(vcolor, 1.0);
         }
 
     |]
